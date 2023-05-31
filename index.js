275109abf39fb721b751ef2d7897d4ed37c0543d
kylechau123 <123038673+kylechau123@users.noreply.github.com>
2023-05-30 22:18:15 -0400

    added more functionality to viewing and updating the database

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ function mainMenu(){
         if(answers.action == "View all departments"){
            viewAllDepartments()
         }if(answers.action == "View all roles"){
-    
+            viewAllRoles()
         }if(answers.action == "View all employees"){
-    
+            viewAllEmployees()
         }if(answers.action == "Add a department"){
             addDepartment()
         }if(answers.action == "Add a role"){
-    
+            addRole()
         }if(answers.action == "Add an employee"){
     
         }if(answers.action == "Update an employee role"){
@@ -54,6 +54,24 @@ function viewAllDepartments(){
     })
 }
 
+function viewAllRoles(){
+    db.query("Select * from role", (err, res) => {
+        console.table(res)
+        setTimeout(() => {
+            mainMenu()
+        }, 5000)
+    })
+}
+
+function viewAllEmployees(){
+    db.query("Select * from employee", (err, res) => {
+        console.table(res)
+        setTimeout(() => {
+            mainMenu()
+        }, 5000)
+    })
+}
+
 function addDepartment(){
     inquirer.prompt([
         {
@@ -63,13 +81,52 @@ function addDepartment(){
         }
     ]).then(answers => {
         db.query("Insert into department(name) values (?)", [answers.departmentName], (err, res) => {
-            console.log("Department Added!")
+            console.log("Department added!")
             setTimeout(() => {
                 mainMenu()
             }, 5000)
-        })
-    })
-}
+        });
+    });
+};
+
+function addRole(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "roleName",
+            message: "What is the name of the new role?"
+        }
+    ]).then(answers => {
+        db.query("Insert into role(title) values (?)", [answers.roleName], (err, res) => {
+            console.log("Role added!")
+            setTimeout(() => {
+                mainMenu()
+            }, 5000)
+        });
+    });
+};
+
+// function addEmployee(){
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "firstName",
+//             message: "What is the first name of the new employee?"
+//         },
+//         {
+//             type: "input",
+//             name: "lastName",
+//             message: "What is the last name of the new employee?"
+//         }
+//     ]).then(answers => {
+//         db.query("Insert into first_name(name) values (?)", [answers.firstName, answers.lastName], (err, res) => {
+//             console.log("Employee added!")
+//             setTimeout(() => {
+//                 mainMenu()
+//             }, 5000)
+//         });
+//     });
+// };
 
 function updateRole(){
     inquirer.prompt([
@@ -83,9 +140,15 @@ function updateRole(){
             name: "roleID",
             message: "What is the new role ID of the employee?"
         }
-    ])//update employee set role_id="(?)" where id = (?)
-}
-
-
+    ]).then(answers => {
+        db.query("update employee set role_id=(?) where id = (?)", [answers.employeeID, answers.roleID], (err, res) => {
+            console.log("Employee updated!")
+            setTimeout(() => {
+                mainMenu()
+            }, 5000)
+        });
+    });
+    //update employee set role_id="(?)" where id = (?)
+};
 
 mainMenu()
